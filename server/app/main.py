8d269e296c2140497074b17cdb34952a833535b0
kzhang0927 <kzhang0927@gmail.com>
2022-03-04 20:26:51 -0500

    Updating data model & API endpoints

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get('/')
-def read_main():
-    return { "message" : "Placeholder"}
+# Hardcoded to localhost
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/offer")
+async def get_alloffers():
+    return "TO-DO to get all offers for dashboard"
+
+@app.get("/api/offer/{offerid}")
+async def get_offer(offerid):
+    return "TO-DO to get specific offer data for offer generation & potential editing later on"
+
+@app.post("/api/offer/")
+async def create_offer():
+    return "TO-DO to create new offer"
--- server/model.py
+++ server/model.py
@@ -1,7 +1,19 @@
+from datetime import date
 from pydantic import BaseModel
 
 #To-do temporary placeholder
 class offers(BaseModel):
-    compensation: str
-    bonus: str
-    benefits: str
+    frequency: str
+    offerID: str
+    create_date: date
+    name: str
+    title: str
+    details: str
+   # fixed_salary: #nested
+   # hourly_wage: #nested
+   # recurring_bonus: #nested
+   # onetime_bonus: #nested
+   # discounts: #nested
+   # perks: #nested
+   # tips: #nested
+   # other: #nested
